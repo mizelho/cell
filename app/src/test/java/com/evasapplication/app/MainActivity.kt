1e496720a550a43c35f96a36f7670464f37a8023
mizelho <evamichele282@gmail.com>
2023-06-25 04:14:46 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/evasapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.evasapplication.app
+
+import com.evasapplication.app.appcomponents.base.BaseActivity
+import com.evasapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
